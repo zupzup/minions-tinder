dfefdfd17c3aabbb7074f3f86a0f890593d274bd
Mario Zupan <mzupanmz@gmail.com>
2015-07-03 09:44:21 +0200

    add test for image

--- scripts/tests/Image.spec.js
+++ scripts/tests/Image.spec.js
@@ -1,9 +1,15 @@
 'use strict';
 
 const React = require('react/addons');
+const TestUtils = React.addons.TestUtils;
+const Image = require('../components/Image');
 
-describe('sanity', () => {
-    it('is sane', () => {
-        expect(true).to.equal(true);
+describe('Image', () => {
+    const endHandler = () => {};
+    const dragHandler = () => {};
+
+    it('renders a draggable image', () => {
+        const res = TestUtils.renderIntoDocument(<Image src='yay' endHandler={endHandler} dragHandler={dragHandler} />);
+        expect(res).not.to.equal(undefined);
     });
 });
