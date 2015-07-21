9f4ceb2a1033453f8e5710db16e4ea6b0f6ef0fc
Mario Zupan <mzupanmz@gmail.com>
2015-07-22 00:03:51 +0200

    init gh-pages

--- scripts/index.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-const React = require('react/addons');
-const App = require('./App');
-
-React.render(<App/>, document.getElementById('app'));
--- test_index.js
+++
@@ -1,7 +0,0 @@
-var chai = require('chai');
-window.expect = chai.expect;
-require('events').EventEmitter.prototype._maxListeners = 0;
-require('core-js/es5');
-var context = require.context('./scripts', true, /spec\.js$/);
-context.keys().forEach(context);
-
